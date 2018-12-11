ff0a4ade5b72aba33b6611d587d73e5b2e82454d
brownme1 <brownme1@rose-hulman.edu>
2018-12-11 00:47:00 -0500

    Project 6

--- src/m1_motion.py
+++ src/m1_motion.py
@@ -2,11 +2,11 @@
 An opportunity to explore how to make an EV3 Robot move.
 
 Authors: Dave Fisher, David Mutchler, Vibha Alangar,
-their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+their colleagues, and Morgan Brown.
+"""  # DONe: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # -----------------------------------------------------------------------------
-# TODO: 2.
+# DONe: 2.
 #   Follow along with the lecture to run this program:
 #    - Using SSH from your computer
 #   When you have successfully run this program, change this _TODO_ to DONE.
@@ -22,7 +22,7 @@ def main():
 
 def go_two_seconds():
     # -------------------------------------------------------------------------
-    # TODO: 3.
+    # DONE: 3.
     #   Make the robot move, by using this pattern:
     #    - Turn on (start) the wheel motors.
     #    - time.sleep(2)  # Pause here, let other processes run for 2 seconds
@@ -38,7 +38,7 @@ def go_two_seconds():
 
 def run_test_go_inches():
     """ Tests the go_inches function. """
-    # TODO: 4.  Implement this function with at least 3 calls to go_inches
+    # DONE: 4.  Implement this function with at least 3 calls to go_inches
     #   with various inches and speeds.
 
 
@@ -49,7 +49,7 @@ def go_inches(inches, percent_of_max_speed):
       :type inches: float
       :type percent_of_max_speed: float  -100 to 100
     """
-    # TODO: 5.  Implement and test this function.
+    # DONE: 5.  Implement and test this function.
 
 
 main()
